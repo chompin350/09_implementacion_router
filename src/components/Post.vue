b989e9d316c28b11020a78f0b83eb28c7b2301b1
chompin350 <chompin@gmail.com>
2021-08-12 21:36:42 -0400

    Desafio 01 router

---
+++ src/components/Post.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+       <h1 v-text="tituloEntrada"></h1>
+       <router-link :to="{name : 'articulo'}">Ver post</router-link>
+        <!-- esta linea debe ir si ponermos un componente dependiente de otro -->
+        <router-view></router-view> 
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+     computed: { 
+        tituloEntrada(){
+            return `Este es el post N° ${this.$route.params.registro} del Blog` }
+    }, 
+}
+</script>
---
+++ src/router.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Portada from './components/Portada'
+import Sobremi from './components/Sobremi'
+import Contacto from './components/Contacto'
+import Post from './components/Post'
+import Articulo from './components/Articulo'
+import Notfound from './components/Notfound'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Portada
+        }, 
+        {
+            path: '/sobremi',
+            component: Sobremi
+        },
+        {
+            path: '/contacto',
+            component: Contacto
+        },
+        {
+            path: '/post/:registro',
+            component: Post,
+            children: [
+                {
+                    path: 'articulo',
+                    component: Articulo,
+                    name: 'articulo'
+                },
+            ]
+        },
+        {
+            path:   '*',
+            component: Notfound
+        }
+    ]
+})
